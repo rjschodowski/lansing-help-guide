fd4247e7aff50efb52eb2f6292fc0c73469a1b48
R.J <rjschodowski@hotmail.com>
2021-04-15 17:41:08 -0400

    commit on 'master'

--- src/pages/food.js
+++ src/pages/food.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Food = () => {
-    <h3>Food</h3>
-
+    return (
+       <div>
+        <h1>Food</h1>
+        <p>Food Pantry</p>
+        <p><span>Website: </span><a href="https://www.lansingmi.gov/530/Mobile-Food-Pantry">Mobile Food Pantry </a></p>
+        <p><span>Phone: </span>(517) 364-1000</p>
+       </div>
+    )
 }
 
 export default Food
