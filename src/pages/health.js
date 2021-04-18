00bc83de4c0f33f56ca9bdba329997dc5b8bfcd7
R.J <rjschodowski@hotmail.com>
2021-04-18 10:05:52 -0400

    commit on 'main'

--- src/pages/health.js
+++ src/pages/health.js
@@ -4,15 +4,62 @@ import React from 'react'
 const Health = () => {
     return (
         <div>
-            <h1>Medical</h1>
+            <h1>Health</h1>
             <h2>Hospitals</h2>
-            <a href="tel:5173719500"><p>Call</p></a>
-            <a href="https://goo.gl/maps/DBHUJ9jPWNPYEoTFA" alt="map directions" target="_blank"><p>Directions</p></a>
-           <a href="https://www.sparrow.org/our-hospitals-and-services/sparrow-hospitals/sparrow-hospital" alt="Sparrow Hospital website link" target="_blank">Website</a>
-           <p>Sparrow Hospital</p>
-           <h2>Clinics</h2>
+            <ul>
+                <li>
+                    <a href="tel:5173719500">Call</a>
+                </li>
+                <li>
+                    <a href="https://goo.gl/maps/DBHUJ9jPWNPYEoTFA" alt="map directions" target="_blank">Directions</a>
+                </li>
+                <li>
+                    <a href="https://www.sparrow.org/our-hospitals-and-services/sparrow-hospitals/sparrow-hospital" alt="Sparrow Hospital website link" target="_blank">Website</a>
+                </li>
+                <li>
+                    <a href="https://www.sparrow.org/our-hospitals-and-services/sparrow-hospitals/sparrow-hospital" alt="Sparrow Hospital website link" target="_blank">Sparrow Hospital</a>
+                </li>
+           </ul>
+           <h2>Medical Clinics</h2>
+           <ul>
+               <li>
+                   <a href="tel:5178875922">Call</a>
+               </li>
+               <li>
+                   <a href="https://goo.gl/maps/MLDaRQ9nnyWvwPzG7" alt="map directions" target="_blank">Directions</a>
+               </li>
+               <li>
+                   <a href="https://carefreemedical.com/" alt="Carefree Medical website link" target="_blank">Website</a>
+               </li>
+                   <a href="https://carefreemedical.com/" alt="Carefree Medical website link" target="_blank">Carefree Medical</a>
+           </ul>
            <h2>COVID-19</h2>
-           <h2>Dental</h2>
+            <ul>
+                <li>
+                    <a href="https://www.solvhealth.com/search?cobrandedSrpLocation=MI" alt="COVID-19 testing search for locations website link" target="_blank">Testing Site Locator</a>
+                </li>
+                <li>
+                    <a href="https://vaccinefinder.org/search/" alt="COVID-19 vaccine finder" target="_blank">Vaccine Finder</a>
+                </li>
+                <li>
+                    <a href="https://www.michigan.gov/" alt="Michigan.gov COVID-19 Information Website" target="_blank">Michigan.gov COVID-19 Information Website</a>
+                </li>
+            </ul>
+            <h2>Dental</h2>
+            <ul>
+                <li>
+                    <a href="tel:5172725053">Call</a>
+                </li>
+                <li>
+                    <a href="https://goo.gl/maps/GGGcXF6xbKG2gVEn8" alt="map directions" target="_blank">Directions</a>
+                </li>
+                <li>
+                    <a href="https://carefreemedical.com/dental/" alt="Carefree Dental website link" target="_blank">Website</a>
+                </li>
+                <li>
+                    <a href="https://carefreemedical.com/dental/" alt="Carefree Dental website link" target="_blank">Carefree Dental</a>
+                </li>
+            </ul>          
         </div>
     )
 }
