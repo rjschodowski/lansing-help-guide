b6169d254b6fd67aea090621dce17feb95a5520d
R.J <rjschodowski@hotmail.com>
2021-04-18 09:19:43 -0400

    commit on 'main'

--- src/pages/health.js
+++ src/pages/health.js
@@ -5,10 +5,14 @@ const Health = () => {
     return (
         <div>
             <h1>Medical</h1>
+            <h2>Hospitals</h2>
             <a href="tel:5173719500"><p>Call</p></a>
             <a href="https://goo.gl/maps/DBHUJ9jPWNPYEoTFA" alt="map directions" target="_blank"><p>Directions</p></a>
            <a href="https://www.sparrow.org/our-hospitals-and-services/sparrow-hospitals/sparrow-hospital" alt="Sparrow Hospital website link" target="_blank">Website</a>
            <p>Sparrow Hospital</p>
+           <h2>Clinics</h2>
+           <h2>COVID-19</h2>
+           <h2>Dental</h2>
         </div>
     )
 }
