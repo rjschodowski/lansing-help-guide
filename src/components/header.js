e5531b08b8acac56e9d746a7d5337fb27b188ecd
R.J <rjschodowski@hotmail.com>
2021-04-19 16:24:17 -0400

    commit on 'main'

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Header = () => {
+    return (
+        <header>
+            <Link to="/">
+                <h3>BACK ICON</h3>
+            </Link>
+            <hr></hr>
+        </header>
+    )
+}
+
+export default Header
