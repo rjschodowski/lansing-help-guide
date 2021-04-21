6986b75877df6b3c0c8c2cf41d1b6402b808939b
R.J <rjschodowski@hotmail.com>
2021-04-21 15:37:05 -0400

    commit on 'main'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 module.exports = {
 
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
