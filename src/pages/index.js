978ac9afd82113e76c8cbd10d5ef235499b88fb2
R.J <rjschodowski@hotmail.com>
2021-04-21 16:05:05 -0400

    commit on 'main'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import Link from 'gatsby-link'
 import HeaderHome from '../components/headerHome'
 
@@ -8,7 +9,11 @@ const IndexPage = () => {
       <HeaderHome />
       <nav>
         <ul>
-          <li><Link to="/food">Food</Link></li>
+          <li><Link to="/food"><StaticImage src="../images/food_bank-24px.svg" 
+                   width={40}
+                   alt="food icon"
+                   placeholder="tracedSVG"
+                   quality="40" />Food</Link></li>
           <li><Link to="/health">Health</Link></li>
           <li><Link to="/shelter">Shelter</Link></li>
           <li><Link to="/safety">Safety</Link></li>
