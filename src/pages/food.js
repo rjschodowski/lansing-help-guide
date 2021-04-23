4996abc101f4eda08ec1a688f57a02bfdcb119ce
R.J <rjschodowski@hotmail.com>
2021-04-23 16:12:35 -0400

    commit on 'main'

--- src/pages/food.js
+++ src/pages/food.js
@@ -1,9 +1,11 @@
 import React from 'react'
 // import Layout from "../components/layout"
+import Header from '../components/header'
 
 const Food = () => {
     return (
        <div>
+           <Header />
         <h1>Food</h1>
         <ul>
             <li>
