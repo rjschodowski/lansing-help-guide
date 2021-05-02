d6454a0d959fb628df61238f73185f11fc85c451
R.J <rjschodowski@hotmail.com>
2021-05-02 14:16:19 -0400

    commit on 'main'

--- src/components/headerHome.js
+++ src/components/headerHome.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
