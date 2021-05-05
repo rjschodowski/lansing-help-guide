f05b9cd48ac5e76c8aa57e84dfef6da91dbdab07
R.J <rjschodowski@hotmail.com>
2021-05-05 16:33:24 -0400

    commit on 'main'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import Link from 'gatsby-link'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import HeaderHome from '../components/headerHome'
 import Button from '@material-ui/core/Button'
 
