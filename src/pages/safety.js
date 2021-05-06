931fad1f4fcb655da04bb64894ab53c968ffb8b8
R.J <rjschodowski@hotmail.com>
2021-05-06 16:51:40 -0400

    commit on 'main'

--- src/pages/safety.js
+++ src/pages/safety.js
@@ -9,50 +9,56 @@ import SmsIcon from '@material-ui/icons/Sms';
 const Safety = () => {
     return (
         <Layout>
-            <ul>
-                <li>
-                    <a href="https://suicidepreventionlifeline.org/">National Suicide Prevention Lifeline</a>
-                </li>
-                <li>
-                    <IconButton aria-label="Call">
-                        <a href="tel:18002738255" alt="call" target="_blank">
-                            <CallIcon />Call
-                        </a>
-                   </IconButton>
-                </li>
-                <li>
-                    <IconButton aria-label="Online chat">
-                        <a href="https://suicidepreventionlifeline.org/chat/" alt="online chat" target="_blank">
-                            <SmsIcon />Online Chat
-                        </a>
+            <div className="location-wrapper">
+                <ul className="flex items-baseline">
+                    <li>
+                        <a href="https://suicidepreventionlifeline.org/">National Suicide Prevention Lifeline</a>
+                    </li>
+                    <li>
+                        <IconButton aria-label="Call">
+                            <a href="tel:18002738255" alt="call" target="_blank">
+                                <CallIcon />Call
+                            </a>
                     </IconButton>
-                </li>
-                <li>
-                    <IconButton aria-label="website">
-                        <a href="https://suicidepreventionlifeline.org/" alt="National Suicide Prevention website link" target="_blank">
-                            <LanguageIcon />Website
-                        </a>
-                    </IconButton>
-                </li>
-                
-                <li>
-                    <a href="https://www.eveinc.org/" alt="COVID-19 testing search for locations website link" target="_blank">End Violent Encounters (EVE) 24 Hour Crisis Line</a>
-                </li>
-                <li>
-                    <IconButton aria-label="Call">
-                        <a href="tel:5173725572" alt="call" target="_blank">
-                            <CallIcon />Call
-                        </a>
-                    </IconButton>
-                </li>
-                <li>
-                    <IconButton>
-                        <a href="https://www.eveinc.org/" alt="End Violent Encounters website link" target="_blank">
-                            <LanguageIcon />Website
-                        </a>
-                    </IconButton>
-                </li>
-            </ul>
+                    </li>
+                    <li>
+                        <IconButton aria-label="Online chat">
+                            <a href="https://suicidepreventionlifeline.org/chat/" alt="online chat" target="_blank">
+                                <SmsIcon />Online Chat
+                            </a>
+                        </IconButton>
+                    </li>
+                    <li>
+                        <IconButton aria-label="website">
+                            <a href="https://suicidepreventionlifeline.org/" alt="National Suicide Prevention website link" target="_blank">
+                                <LanguageIcon />Website
+                            </a>
+                        </IconButton>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="location-wrapper">    
+                <ul className="flex items-baseline">
+                    <li>
+                        <a href="https://www.eveinc.org/" alt="COVID-19 testing search for locations website link" target="_blank">End Violent Encounters (EVE) 24 Hour Crisis Line</a>
+                    </li>
+                    <li>
+                        <IconButton aria-label="Call">
+                            <a href="tel:5173725572" alt="call" target="_blank">
+                                <CallIcon />Call
+                            </a>
+                        </IconButton>
+                    </li>
+                    <li>
+                        <IconButton>
+                            <a href="https://www.eveinc.org/" alt="End Violent Encounters website link" target="_blank">
+                                <LanguageIcon />Website
+                            </a>
+                        </IconButton>
+                    </li>
+                </ul>
+                </div>
         </Layout>
     )
 }
