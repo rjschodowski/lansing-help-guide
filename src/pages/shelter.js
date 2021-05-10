78fd5ced5e2ab3f7f36fd8513bd512a07845452f
R.J <rjschodowski@hotmail.com>
2021-05-10 14:47:38 -0400

    Adjusted flexbox row layout, margin, padding, text size for location names for shelter page commit on 'main'

--- src/pages/shelter.js
+++ src/pages/shelter.js
@@ -9,29 +9,29 @@ import LanguageIcon from '@material-ui/icons/Language';
 const Shelter = () => {
     return (
         <Layout>
-            <div className="location-wrapper">
+            <div className="location-wrapper -mb-10">
                 <ul className="flex items-baseline">
-                    <li>
-                        <a href="https://bearescuer.org" alt="City Rescue Mission website link" target="_blank">City Rescue Mission</a>
+                    <li className="flex-grow pr-8">
+                        <a href="https://bearescuer.org" alt="City Rescue Mission website link" target="_blank" className="text-sm">City Rescue Mission</a>
                     </li>
                     <li>
                     <IconButton aria-label="Call">
                             <a href="tel:5174850145" alt="call" target="_blank">
-                                <CallIcon />Call
+                                <CallIcon />
                             </a>
                     </IconButton>
                 </li>
                 <li>
                     <IconButton>
                             <a href="https://goo.gl/maps/BkwfojRACQ7NWCGJA" alt="map directions" target="_blank">
-                                <DirectionsIcon />Directions
+                                <DirectionsIcon />
                             </a>
                     </IconButton>
                     </li>
                 <li>
                     <IconButton>
                             <a href="https://bearescuer.org" alt="City Rescue Mission website link" target="_blank">
-                                <LanguageIcon />Website
+                                <LanguageIcon />
                             </a>
                         </IconButton>
                     </li>
