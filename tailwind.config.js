807a4896dc6b426429da8b757ed4500d712a7201
R.J <rjschodowski@hotmail.com>
2021-05-26 16:16:22 -0400

    iconcategories.js component added to purge array in tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/components/header.js', './src/components/headerHome.js', './src/components/layout.js', './src/pages/index.js', './src/pages/financial.js', './src/pages/food.js', './src/pages/health.js', './src/pages/legal.js', './src/pages/safety.js', './src/pages/shelter.js'],
+  purge: ['./src/components/header.js', './src/components/iconcategories.js', './src/components/headerHome.js', './src/components/layout.js', './src/pages/index.js', './src/pages/financial.js', './src/pages/food.js', './src/pages/health.js', './src/pages/legal.js', './src/pages/safety.js', './src/pages/shelter.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
