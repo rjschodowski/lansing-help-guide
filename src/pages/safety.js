fa576d2612375a963ea4d98e3649183123aa62d4
R.J <rjschodowski@hotmail.com>
2021-05-27 13:02:28 -0400

    Subheadings' margins adjusted on Safety.js page

--- src/pages/safety.js
+++ src/pages/safety.js
@@ -150,7 +150,7 @@ const Safety = () => {
         </ul>
       </div>
 
-      <h4 className="text-blue-500 ml-4 mr-4 mb-2">Human Trafficking Help</h4>
+      <h4 className="text-blue-500 ml-4 mb-2">Human Trafficking Help</h4>
       <div className="location-wrapper -mb-10">
         <ul className="flex items-baseline">
           <li className="flex-grow">
