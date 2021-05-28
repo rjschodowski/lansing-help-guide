92459bb08be98b3004af663b291a73a715a0241e
R.J <rjschodowski@hotmail.com>
2021-05-28 10:31:40 -0400

    Bubblewrap-related commit

---
+++ app/src/main/java/app/netlify/lansing_community_resources/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.lansing_community_resources.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
